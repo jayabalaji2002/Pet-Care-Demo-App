f1a38b262f13f31e3d08b6d982d2b25aba804e81
Jayabalaji <111067200+jayabalaji2002@users.noreply.github.com>
2024-08-06 18:35:37 +0530

    Contact us page design started

--- src/Component/Contact/Contact.js
+++ src/Component/Contact/Contact.js
@@ -1,9 +1,22 @@
 import React from 'react'
-// import contact from "./Contact.module.css"
+import ctct from "./Contact.module.css"
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className={ctct.contact_page}>
+      <div className={ctct.header_txt}>
+        <div className={ctct.htxt_background}>
+          <h1 className={ctct.text_h1_header}>Contact</h1>
+        </div>
+      </div>
+
+
+      <div className={ctct.contact_container}>
+        <div className={ctct.contact_section}>
+        </div>
+      </div>
+      
+    </div>
   )
 }
 
