6bed0d851b125addeaa5caec6ca6361c0675d785
Jayabalaji <111067200+jayabalaji2002@users.noreply.github.com>
2024-08-06 12:51:04 +0530

    First commit from service page branch to Develop

--- src/Component/Services/Services.js
+++ src/Component/Services/Services.js
@@ -1,9 +1,15 @@
 import React from 'react'
-// import contact from './Services.module.css'
+import svs from './Services.module.css'
 
 function Services() {
   return (
-    <div>Services</div>
+    <div className={svs.services_page}>
+      <div className={svs.header_txt}>
+        <div className={svs.htxt_background}>
+           <h1 className={svs.text_h1_header}>Services</h1>     
+        </div>
+      </div>
+    </div>
   )
 }
 
